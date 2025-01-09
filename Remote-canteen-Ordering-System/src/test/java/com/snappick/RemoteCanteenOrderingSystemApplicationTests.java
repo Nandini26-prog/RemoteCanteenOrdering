069d45084fc7399b6ba8ea2d59705997fc4deecf
Nandini Jain <nandini.nj26@gmail.com>
2025-01-09 21:47:18 +0530

    Packages created for database initialisation and entry todo:connect postgres and create the required tables in it.

---
+++ Remote-canteen-Ordering-System/src/test/java/com/snappick/RemoteCanteenOrderingSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.snappick;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RemoteCanteenOrderingSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
